8be1544c4903fc37fae390b0c2a2602db895f3c4
ThomazPom <thomas.benhamou@hotmail.fr>
2018-07-26 14:47:45 +0200

    1.2 : [Added regexp based whitelist]

---
+++ popup.js
@@ -0,0 +1,20 @@
+// content-script.js
+var defaultRgx =  ["http://*/*", "https://*/*"].join('\n')
+var myPort = browser.runtime.connect({name:"port-from-cs"});
+myPort.onMessage.addListener(function(m) {
+  document.querySelector("#warning").innerText=m.message
+});
+browser.storage.local.get("regstr", function(res) {
+  regstr = (res.regstr || defaultRgx);
+  document.querySelector(".listextarea").value=regstr;
+});
+window.onload= function()
+{
+  txarea = document.querySelector(".listextarea");
+  txarea.onkeyup = txarea.onchange = function(){
+    regstr = txarea.value.trim()
+    browser.storage.local.set({"regstr": regstr}, function() {
+      myPort.postMessage({updateRegexpes: regstr});
+    });
+  }
+}
