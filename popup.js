d0b5c34a9902c36374bf61ccc9394601f68c7898
ThomazPom <Thomas.benhamou@hotmail.fr>
2019-07-28 14:56:41 +0200

    1.5 : Added parent domain whitelist

--- popup.js
+++ popup.js
@@ -1,18 +1,28 @@
-// content-script.js
 var defaultRgx =  ["http://*/*", "https://*/*"].join('\n')
+var defaultRgx_fancestor =  ["http://*", "https://*"].join('\n')
+
 var myPort = browser.runtime.connect({name:"port-from-cs"});
-myPort.onMessage.addListener(function(m) {
-  document.querySelector("#warning").innerText=m.message
+
+
+browser.storage.local.get("regstr_fancestor", function(res) {
+  var regstr_fancestor = (res.regstr_fancestor || defaultRgx_fancestor);
+  document.querySelector(".listextarea_fancestor").value=regstr_fancestor;
 });
 browser.storage.local.get("regstr", function(res) {
-  regstr = (res.regstr || defaultRgx);
+  var regstr = (res.regstr || defaultRgx);
   document.querySelector(".listextarea").value=regstr;
 });
 window.onload= function()
 {
   txarea = document.querySelector(".listextarea");
-  txarea.onkeyup = txarea.onchange = function(){
+  txarea_fancestor = document.querySelector(".listextarea_fancestor");
+  txarea.onkeyup = txarea.onchange = txarea_fancestor.onkeyup = txarea_fancestor.onchange
+  = function(){
     regstr = txarea.value.trim()
-    myPort.postMessage({updateRegexpes: regstr}); 
+    regstr_fancestor = txarea_fancestor.value.trim()
+    myPort.postMessage({
+    	updateRegexpes: regstr,
+    	updateRegexpes_fancestor:regstr_fancestor
+    }); 
   }
 }
