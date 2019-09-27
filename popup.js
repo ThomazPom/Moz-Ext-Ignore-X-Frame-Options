f383fe3c9aa6feb931f15d71a11eaed4eb44cd46
ThomazPom <thomas.benhamou@hotmail.fr>
2019-09-28 00:20:33 +0200

    append rather than replace

--- popup.js
+++ popup.js
@@ -12,17 +12,30 @@ browser.storage.local.get("regstr", function(res) {
   var regstr = (res.regstr || defaultRgx);
   document.querySelector(".listextarea").value=regstr;
 });
+browser.storage.local.get("appendrather", function(res) {
+
+  if(res.appendrather)
+  {
+    document.querySelector("#appendrather").setAttribute("checked","checked");
+  }
+});
 window.onload= function()
 {
   txarea = document.querySelector(".listextarea");
   txarea_fancestor = document.querySelector(".listextarea_fancestor");
-  txarea.onkeyup = txarea.onchange = txarea_fancestor.onkeyup = txarea_fancestor.onchange
+  appendrather = document.querySelector("#appendrather");
+  
+  
+  appendrather.onclick = txarea.onkeyup = txarea.onchange = txarea_fancestor.onkeyup = txarea_fancestor.onchange
   = function(){
+    console.log(appendrather.checked );
     regstr = txarea.value.trim()
     regstr_fancestor = txarea_fancestor.value.trim()
     myPort.postMessage({
     	updateRegexpes: regstr,
-    	updateRegexpes_fancestor:regstr_fancestor
+      updateRegexpes_fancestor:regstr_fancestor,
+      appendrather:appendrather.checked 
     }); 
+
   }
 }
